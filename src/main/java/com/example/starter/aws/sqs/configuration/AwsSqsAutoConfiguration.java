7132e9804cd9def5b2cc133f9cd48b9e2b7fb579
zyf <zhaoyunfeng@qiyi.com>
2023-10-05 09:39:40 +0100

    Update AwsSqsAutoConfiguration.java

--- src/main/java/com/example/starter/aws/sqs/configuration/AwsSqsAutoConfiguration.java
+++ src/main/java/com/example/starter/aws/sqs/configuration/AwsSqsAutoConfiguration.java
@@ -1,17 +1,11 @@
 package com.example.starter.aws.sqs.configuration;
 
-import com.example.starter.aws.sqs.aws.AwsSqsClientProxy;
 import com.example.starter.aws.sqs.client.AwsSqsClientRegistrar;
 import com.example.starter.aws.sqs.properties.AwsSqsProperties;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 
 /**
  * Entrance autoConfiguration of the starter
@@ -23,7 +17,7 @@ import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
         prefix = "aws.sqs",
         name = "enabled"
 )
-@Import({AwsSqsClientRegistrar.class, AwsSqsProperties.class})
+@Import({AwsSqsClientRegistrar.class})
 public class AwsSqsAutoConfiguration {
 
     @Bean
